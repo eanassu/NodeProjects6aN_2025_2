3669bbbbe11983fc65e00eaf01e958fc708a08bc
eanassu <eanassu@gmail.com>
2025-10-24 20:05:47 -0300

    página de listagem de funcionários

--- expressApp/controllers/funcionarioController.js
+++ expressApp/controllers/funcionarioController.js
@@ -1,5 +1,14 @@
+const Funcionario=require('../models/funcionarioModel');
+
 exports.inicio = (req,res)=>res.render('funcionarios/index');
 exports.cadastro = (req,res)=>res.render('funcionarios/cadastro');
 exports.exclusao = (req,res)=>res.render('funcionarios/exclusao');
 exports.busca = (req,res)=>res.render('funcionarios/busca');
-exports.lista = (req,res)=>res.render('funcionarios/lista');
+
+exports.lista = (req,res) => {
+    Funcionario.getAll((err,results) => {
+        if (err) 
+            return res.status(500).send('Erro ao buscar Funcionários');
+        res.render('funcionarios/lista',{funcionarios:results});
+    });
+};
